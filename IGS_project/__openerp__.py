8bda044c0a9f07466c017a1fef351fd9ce731e1b
shweyi1989 <winmyashweyi@gmail.com>
2016-08-08 10:44:26 +0630

    for invoice template customize

---
+++ IGS_project/__openerp__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'IGS',
+    'version': '1.0',
+    'category': 'IGS',
+    'sequence': 14,
+    'summary': 'IGS',
+    'description': """
+IGS Management Module
+===============
+
+IGS Management module.
+
+    """,
+    'author': 'Win Mya Shwe Yi',
+    'website': 'http://www.infinityglobals.com',
+    'images': [],
+    'depends': [],
+    'data': [
+        'views/invoice_report.xml', 'views/igs_external_header.xml',
+        'views/igs_external_footer.xml',
+         ],
+    'demo': [
+    ],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}

938ecfab6da348fddb1c1449f974ba497ab17ee5
shweyi1989 <winmyashweyi@gmail.com>
2016-08-24 11:29:12 +0630

    for Tianyu Solution project, change 'Powered by Odoo' to 'Powered by IGS'

---
+++ Tianyu_Project/__openerp__.py
@@ -0,0 +1,31 @@
+{
+    'name': 'Tianyu Solution',
+    'version': '1.0',
+    'category': 'Tianyu Solution',
+    'sequence': 14,
+    'summary': 'Tianyu Solution',
+    'description': """
+Tianyu Solution Module
+===============
+
+Tianyu Solution  module.
+
+    """,
+    'author': 'IGS',
+    'website': 'http://www.infinityglobals.com',
+    'images': [],
+    'depends': [],
+    'data': [
+           'views/igs_login_page.xml','views/igs_external_footer.xml',
+           
+           
+         ],
+    'demo': [
+    ],
+    'css': [],
+    'js' : [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
